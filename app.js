aea7fcfab7939e4054af82cc64b349f26552da77
amanfarhan2008@gmail.com <amanfarhan2008@gmail.com>
2025-06-29 16:26:37 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// let a = 3 ;
+// let b = 5 ;
+// let sum = a+ b;
+// console.log(a+b)
+
+
+let userInput = prompt("Enter num ");
+let a = 1;
+console.log(userInput + "x" + a +"=" + userInput *a)
+console.log(userInput + "x" + ++a +"=" + userInput *a)
+console.log(userInput + "x" + ++a +"=" + userInput *a)
+console.log(userInput + "x" + ++a +"=" + userInput *a)
+console.log(userInput + "x" + ++a +"=" + userInput *a)
+console.log(userInput + "x" + ++a +"=" + userInput *a)
+console.log(userInput + "x" + ++a +"=" + userInput *a)
+console.log(userInput + "x" + ++a +"=" + userInput *a)
+console.log(userInput + "x" + ++a +"=" + userInput *a)
+console.log(userInput + "x" + ++a +"=" + userInput *a)
+console.log(userInput + "x" + ++a +"=" + userInput *a)
+console.log(userInput + "x" + ++a +"=" + userInput *a)
