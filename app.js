1896ce5ff63a0d4afffd1acdc3750780e2295df3
amanfarhan2008@gmail.com <amanfarhan2008@gmail.com>
2025-06-29 16:30:49 +0500

    abs

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-// let a = 3 ;
-// let b = 5 ;
-// let sum = a+ b;
-// console.log(a+b)
+let a = 3 ;
+let b = 5 ;
+let sum = a+ b;
+console.log(a+b)
 
 
-let userInput = prompt("Enter num ");
-let a = 1;
-console.log(userInput + "x" + a +"=" + userInput *a)
-console.log(userInput + "x" + ++a +"=" + userInput *a)
-console.log(userInput + "x" + ++a +"=" + userInput *a)
-console.log(userInput + "x" + ++a +"=" + userInput *a)
-console.log(userInput + "x" + ++a +"=" + userInput *a)
-console.log(userInput + "x" + ++a +"=" + userInput *a)
-console.log(userInput + "x" + ++a +"=" + userInput *a)
-console.log(userInput + "x" + ++a +"=" + userInput *a)
-console.log(userInput + "x" + ++a +"=" + userInput *a)
-console.log(userInput + "x" + ++a +"=" + userInput *a)
-console.log(userInput + "x" + ++a +"=" + userInput *a)
-console.log(userInput + "x" + ++a +"=" + userInput *a)
+// let userInput = prompt("Enter num ");
+// let a = 1;
+// console.log(userInput + "x" + a +"=" + userInput *a)
+// console.log(userInput + "x" + ++a +"=" + userInput *a)
+// console.log(userInput + "x" + ++a +"=" + userInput *a)
+// console.log(userInput + "x" + ++a +"=" + userInput *a)
+// console.log(userInput + "x" + ++a +"=" + userInput *a)
+// console.log(userInput + "x" + ++a +"=" + userInput *a)
+// console.log(userInput + "x" + ++a +"=" + userInput *a)
+// console.log(userInput + "x" + ++a +"=" + userInput *a)
+// console.log(userInput + "x" + ++a +"=" + userInput *a)
+// console.log(userInput + "x" + ++a +"=" + userInput *a)
+// console.log(userInput + "x" + ++a +"=" + userInput *a)
+// console.log(userInput + "x" + ++a +"=" + userInput *a)
